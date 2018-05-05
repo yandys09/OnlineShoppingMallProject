294ad8f07fc7d17c278b727750b72d6932dd0425
yandys09 <yandys03@naver.com>
2018-05-05 13:52:57 +0900

    First Settings

---
+++ shoppingbackend/src/main/java/com/tway/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.tway.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
