0db7531bcba8968f43e1544237c77e8f86154537
yandys09 <yandys03@naver.com>
2018-05-22 18:35:22 +0900

    qq

--- shoppingbackend/src/main/java/com/tway/shoppingbackend/config/HibernateConfig.java
+++ shoppingbackend/src/main/java/com/tway/shoppingbackend/config/HibernateConfig.java
@@ -26,23 +26,24 @@ public class HibernateConfig {
 	private final static String DATABASE_USERNAME = "sa";
 	private final static String DATABASE_PASSWORD = "";
 	
-
-	
+	// dataSource bean will be available
 	@Bean("dataSource")
-	private DataSource getDataSource() {
+	public DataSource getDataSource() {
 		
 		BasicDataSource dataSource = new BasicDataSource();
 		
+		// Providing the database connection information
 		dataSource.setDriverClassName(DATABASE_DRIVER);
 		dataSource.setUrl(DATABASE_URL);
 		dataSource.setUsername(DATABASE_USERNAME);
 		dataSource.setPassword(DATABASE_PASSWORD);
+				
 		
 		return dataSource;
 		
 	}
 	
-	//sessioinFactory bean will be available
+	// sessionFactory bean will be available
 	
 	@Bean
 	public SessionFactory getSessionFactory(DataSource dataSource) {
@@ -55,25 +56,29 @@ public class HibernateConfig {
 		return builder.buildSessionFactory();
 		
 	}
-	
-	
 
-	//All the hibernate properties will be returned in this method
+	
+	
+	// All the hibernate properties will be returned in this method	
 	private Properties getHibernateProperties() {
 		
 		Properties properties = new Properties();
-		properties.put("hibernate.dialect",  DATABASE_DIALECT);
-		properties.put("hibernate.show_sql",  "true");
+		
+		
+		properties.put("hibernate.dialect", DATABASE_DIALECT);		
+		properties.put("hibernate.show_sql", "true");
 		properties.put("hibernate.format_sql", "true");
 		
-		return null;
+		//properties.put("hibernate.hbm2ddl.auto", "create");
+		
+		
+		return properties;
 	}
 	
+	// transactionManager bean
 	@Bean
 	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
-		
 		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
-		
 		return transactionManager;
 	}
 	
